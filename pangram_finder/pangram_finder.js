20074ea9a36d16589bb7dcdf6820005e3880d5be
Serenity911 <silvia.simonassi01@gmail.com>
2020-01-08 22:15:57 +0000

    pangram tests works

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,16 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
-
+// phrase is a string = 'the quick brown fox jumps over the lazy dog'
 PangramFinder.prototype.isPangram = function () {
 
-}
+  const result = this.alphabet.every((letter) => {
+    return this.phrase.toLowerCase().includes(letter)
+  })
 
+console.log("THIS IS LOGGING RESULT", result);
+
+  return result
+}
 module.exports = PangramFinder;
