c23d0b7ecaeed844fd2d2dda42b1d6bb6e8e8fb7
Serenity911 <silvia.simonassi01@gmail.com>
2020-01-08 22:46:42 +0000

    isogram tests passed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,13 @@
 const IsogramFinder = function (word) {
-
+  this.word = word;
 }
+// An isogram is a word, phrase or sentence that does not contain any repeated letters.
 
+// example of word, a string 'subdermatoglyphic'
 IsogramFinder.prototype.isIsogram = function () {
 
+  const checkArray = new Set(this.word);
+  return checkArray.size === this.word.length;
 }
 
 module.exports = IsogramFinder;
